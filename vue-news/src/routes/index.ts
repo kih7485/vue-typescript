50657c0d867134d3b30395db7e2da2fafba18840
inhan <korea4127@gmail.com>
2022-01-04 20:25:31 +0900

    news 프로젝트에 typescript 추가

---
+++ vue-news/src/routes/index.ts
@@ -0,0 +1,84 @@
+import Vue from "vue";
+import VueRouter, { NavigationGuardNext, Route } from "vue-router";
+import { ItemView, UserView } from "../views";
+import createListView from "../views/CreateListView";
+import bus from "../utils/bus";
+import store from "../store/index.js";
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      redirect: "/news",
+    },
+    {
+      path: "/news",
+      name: "news",
+      component: createListView("NewsView"),
+      beforeEnter(routeTo, routeFrom, next) {
+        bus.$emit("on:progress");
+        store
+          .dispatch("FETCH_LIST", routeTo.name)
+          .then(() => next())
+          .catch(() => new Error("failed to fetch news items"));
+      },
+    },
+    {
+      path: "/ask",
+      name: "ask",
+      component: createListView("AskView"),
+      async beforeEnter(
+        routeTo: Route,
+        routeFrom: Route,
+        next: NavigationGuardNext<Vue>
+      ) {
+        try {
+          bus.$emit("on:progress");
+          await store.dispatch("FETCH_LIST", routeTo.name);
+          next();
+        } catch (error) {
+          console.error(error);
+        }
+      },
+    },
+    {
+      path: "/jobs",
+      name: "jobs",
+      component: createListView("JobsView"),
+      beforeEnter(routeTo, routeFrom, next) {
+        bus.$emit("on:progress");
+        store
+          .dispatch("FETCH_LIST", routeTo.name)
+          .then(() => next())
+          .catch(() => new Error("failed to fetch news items"));
+      },
+    },
+    {
+      path: "/item/:id",
+      component: ItemView,
+      beforeEnter(routeTo, routeFrom, next) {
+        bus.$emit("on:progress");
+        const itemId = routeTo.params.id;
+        store
+          .dispatch("FETCH_ITEM", itemId)
+          .then(() => next())
+          .catch((err) => new Error("failed to fetch item details"));
+      },
+    },
+    {
+      path: "/user/:id",
+      component: UserView,
+      beforeEnter(routeTo, routeFrom, next) {
+        bus.$emit("on:progress");
+        const itemId = routeTo.params.id;
+        store
+          .dispatch("FETCH_USER", itemId)
+          .then(() => next())
+          .catch((err) => new Error("failed to fetch user profile"));
+      },
+    },
+  ],
+});
